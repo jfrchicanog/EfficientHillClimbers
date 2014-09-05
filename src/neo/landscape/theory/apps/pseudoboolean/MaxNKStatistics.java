2d1202e275c92260747edaf22a6cf30858c729ad
francis <francis>
2014-09-05 08:53:07 +0000

    Some concerns added

--- src/neo/landscape/theory/apps/pseudoboolean/MaxNKStatistics.java
+++ src/neo/landscape/theory/apps/pseudoboolean/MaxNKStatistics.java
@@ -1,10 +1,21 @@
 package neo.landscape.theory.apps.pseudoboolean;
 
+/**
+ * 
+ * @author Francisco Chicano
+ * @email  chicano@lcc.uma.es
+ * This class should compute the probability distribution of the maximum value 
+ * for an NK Landscape. However, I think the implementation is wrong.
+ * It could be also the case that this computation is used only for the adjacent model.
+ * Definitely, I think th distribution is built for the K=1 adjacent model.
+ */
+
 public class MaxNKStatistics {
 
 	private int n;
 	private int q;
 	
+	/** This is the accumulated probability distribution */
 	private double [] distribution;
 	
 	public void setConfiguration(int n, int q)
@@ -13,6 +24,9 @@ public class MaxNKStatistics {
 		this.q=q;
 	}
 	
+	/**
+	 * This probability distribution is valid only for K=1 adjacent NK-landscape
+	 */
 	public void computeDistribution()
 	{
 		distribution = new double [n*(q-1)+1];
@@ -35,12 +49,16 @@ public class MaxNKStatistics {
 			for (int j=f*(q-1)-1; j>= 0; j--)
 			{
 				// Compute the average over the last q terms
+				// This has the effect of computing the probability distirbution
+				// of a sum of two random variables where one is uniformly distributed with q values
 				double sum=0;
 				for (int k=j; k > Math.max(-1, j-q); k--)
 				{
 					sum += distribution[k];
 				}
 				sum /= q;
+				// The square has the effect of computing the probability distribution of
+				// the maximum of two random variables distributed with the distribution computed
 				sum *= sum;
 				distribution[j]=sum;
 			}
