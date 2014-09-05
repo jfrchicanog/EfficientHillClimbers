9d977d50047b27a17f434a9dbbc9158ee9a23f5e
francis <francis>
2014-09-05 08:53:32 +0000

    Some more changes

--- src/neo/landscape/theory/apps/pseudoboolean/experiments/LocalOptimaExperiment.java
+++ src/neo/landscape/theory/apps/pseudoboolean/experiments/LocalOptimaExperiment.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
@@ -20,10 +22,32 @@ import neo.landscape.theory.apps.util.Seeds;
 
 public class LocalOptimaExperiment implements IExperiment {
 	
+	private class SolutionFrequency{
+		public PBSolution sol;
+		public double val;
+		public int frequency;
+		
+		public SolutionFrequency (PBSolution s, int freq)
+		{
+			this.sol=s;
+			this.frequency = freq;
+			val = pbf.evaluate(sol);
+		}
+
+	}
+	
+	
 	private List<PBSolution> localOptima;
 	private PrintWriter nodesFile;
 	private PrintWriter edgesFile;
+	private PrintWriter histogramFile;
+	private PrintWriter gpProgram; 
 	private Set<Integer> appearedEdges;
+	
+	private int [] loHistogram;
+	
+	private NKLandscapes pbf;
+	private int r;
 	
 	public LocalOptimaExperiment()
 	{
@@ -72,7 +96,7 @@ public class LocalOptimaExperiment implements IExperiment {
 		String k = args[1];
 		String q = args[2];
 		String circular = args[3];
-		int r = Integer.parseInt(args[4]);
+		r = Integer.parseInt(args[4]);
 		long seed = 0;
 		if (args.length >= 6)
 		{
@@ -84,7 +108,7 @@ public class LocalOptimaExperiment implements IExperiment {
 		}
 		
 		
-		NKLandscapes pbf = new NKLandscapes();
+		pbf = new NKLandscapes();
 		Properties prop = new Properties();
 		prop.setProperty(NKLandscapes.N_STRING, n);
 		prop.setProperty(NKLandscapes.K_STRING, k);
@@ -105,6 +129,8 @@ public class LocalOptimaExperiment implements IExperiment {
 			nodesFile = new PrintWriter(new FileOutputStream(file_name+".nodes"));
 			nodesFile.println("FITNESS");
 			edgesFile = new PrintWriter(new FileOutputStream(file_name+".edges"));
+			histogramFile = new PrintWriter(new FileOutputStream(file_name+".hist"));
+			gpProgram = new PrintWriter(new FileOutputStream(file_name+".gp"));
 		}
 		catch (FileNotFoundException e)
 		{
@@ -134,6 +160,7 @@ public class LocalOptimaExperiment implements IExperiment {
 		
 		long final_time = System.currentTimeMillis();
 		
+		loHistogram = new int [localOptima.size()];
 		// Applying PX to all the pair of LO
 		PartitionCrossover px = new PartitionCrossover(pbf);
 		
@@ -147,6 +174,10 @@ public class LocalOptimaExperiment implements IExperiment {
 			}
 		}
 		
+		long crossover = System.currentTimeMillis();
+		
+		writeHistogram();
+		writeGNUPlotProgram(file_name);
 		
 		int max_app=0;
 		int max_interactions=0;
@@ -166,6 +197,7 @@ public class LocalOptimaExperiment implements IExperiment {
 		System.out.println("Problem init time: "+rball.getProblemInitTime());
 		System.out.println("Solution init time: "+rball.getSolutionInitTime());
 		System.out.println("Move time: "+(final_time-init_time));
+		System.out.println("Move+crossover time: "+(crossover-init_time));
 		System.out.println("Stored scores:"+rball.getStoredScores());
 		System.out.println("Var appearance (max):"+max_app);
 		System.out.println("Var interaction (max):"+max_interactions);
@@ -173,9 +205,46 @@ public class LocalOptimaExperiment implements IExperiment {
 		pbf.writeTo(new OutputStreamWriter(System.out));
 		nodesFile.close();
 		edgesFile.close();
+		histogramFile.close();
+		gpProgram.close();
 
 	}
 	
+	private void writeHistogram() {
+		
+		List<SolutionFrequency> aux = new ArrayList<>();
+		int index =0;
+		for (PBSolution sol: localOptima)
+		{
+			aux.add(new SolutionFrequency(sol, loHistogram[index++]));
+		}
+		
+		Collections.sort(aux, new Comparator<SolutionFrequency>(){
+			@Override
+			public int compare(SolutionFrequency o1, SolutionFrequency o2) {
+				return -Double.compare(o1.val, o2.val);
+			}
+		});
+		
+		for (SolutionFrequency sf: aux)
+		{
+			histogramFile.println(sf.val+" "+sf.frequency);
+		}
+		
+	}
+	
+	private void writeGNUPlotProgram(String fileName) {
+		gpProgram.println("set style histogram gap 5");
+		gpProgram.println("set style data histograms");
+		gpProgram.println("set style fill solid 1.0 border -1");
+		gpProgram.println("set boxwidth 0.9 absolute");
+		gpProgram.println("set xrange [0 : "+localOptima.size()+"]");
+		gpProgram.println("set terminal postscript color solid");
+		gpProgram.println("set output '"+fileName+".ps'");
+		gpProgram.println("plot '"+fileName+".hist' using 2");
+		gpProgram.println("exit");
+	}
+
 	private String wI(int i)
 	{
 		return ""+(i+1);
@@ -201,17 +270,52 @@ public class LocalOptimaExperiment implements IExperiment {
 
 		if (index < 0 || (index != i && index != j))
 		{
-			System.out.println("R:"+localOptima.get(i)+"(" + wI(i) +
+			System.out.print("R:"+localOptima.get(i)+"(" + wI(i) +
 					") x "+localOptima.get(j)+"(" + wI(j) +
 					") -> "+res + (index < 0?"":"(" + wI(index) +
 							")"));
+			
+			if (index < 0)
+			{
+				res = climbToLocalOptima(res);
+				index = localOptima.indexOf(res);
+				
+				if (index >= 0)
+				{
+					System.out.print(" -> "+res + "(" + wI(index) +")");
+				}
+				else
+				{
+					System.out.print ("Local Optima not found after climbing");
+				}
+			}
+			
+			
+			System.out.println();
 			
 			if (index >= 0)
 			{
 				notifyEdge(i, index);
 				notifyEdge(j, index);
+				
+				loHistogram[index]++;
 			}
+			
 		}
+	}
+
+	private PBSolution climbToLocalOptima(PBSolution res) {
+		RBallEfficientHillClimber rball = new RBallEfficientHillClimber(r);
+		rball.initialize(pbf, res);
+		
+		double imp;
+		do
+		{
+			imp = rball.move();
+			
+		} while (imp > 0);
+		
+		return rball.getSolution();
 	}
 
 }
