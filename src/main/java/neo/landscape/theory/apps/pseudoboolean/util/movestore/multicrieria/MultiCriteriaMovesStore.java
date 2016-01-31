77fd8b84526caa3f3f15f165877ba1252e60574a
francis <chicano@lcc.uma.es>
2016-01-31 23:45:22 +0100

    Created multi criteria move store

---
+++ src/main/java/neo/landscape/theory/apps/pseudoboolean/util/movestore/multicrieria/MultiCriteriaMovesStore.java
@@ -0,0 +1,23 @@
+package neo.landscape.theory.apps.pseudoboolean.util.movestore.multicrieria;
+
+
+public interface MultiCriteriaMovesStore<M> {
+
+    public Iterable<M> iterableOverMoves();
+    public int getNumberOfMoves();
+    public M getMoveByID(int entryIndex);
+    
+    public Iterable<M> iterableOverBucket(int criterion, int radius, int bucket);
+    public int getNumberOfBuckets(int criterion, int radius);
+    
+    public void changeMoveBucket(int criterion, int radius, int oldBucket, int newBucket, M move);
+    
+    public boolean isBucketEmpty(int criterion, int radius, int bucket);
+    public int sizeOfBucket(int criterion, int radius, int bucket);
+    
+    public M getDeterministicMove(int criterion, int radius, int bucket);
+    public M getRandomMove(int criterion, int radius, int bucket);
+    public M getRandomMove(int criterion, int radius, int fromBucket, int toBucket);
+    public abstract int getBucketOfMove(int criterion, M move);
+
+}
