dfc870c66eae6b317eca33a235836182bcb7ecd8
francis <chicano@lcc.uma.es>
2018-07-15 00:10:27 +0900

    Renaming the algorithms for branding purposes

---
+++ src/main/java/neo/landscape/theory/apps/pseudoboolean/experiments/DrilsApxMAXSATExperiment.java
@@ -0,0 +1,9 @@
+package neo.landscape.theory.apps.pseudoboolean.experiments;
+
+public class DrilsApxMAXSATExperiment extends DrilsPXAPMAXSATExperiment {
+    @Override
+    public String getID() {
+        return "drils+apx-maxsat";
+    }
+
+}
---
+++ src/main/java/neo/landscape/theory/apps/pseudoboolean/experiments/DrilsApxNKLandscapesExperiment.java
@@ -0,0 +1,8 @@
+package neo.landscape.theory.apps.pseudoboolean.experiments;
+
+public class DrilsApxNKLandscapesExperiment extends DrilsPXAPNKLandscapesExperiment {
+    @Override
+    public String getID() {
+        return "drils+apx-nk";
+    }
+}
