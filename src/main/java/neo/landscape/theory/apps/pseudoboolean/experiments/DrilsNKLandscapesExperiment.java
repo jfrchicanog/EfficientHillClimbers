4514411620dcd827da09bafb27af63732701a81e
francis <chicano@lcc.uma.es>
2019-06-22 02:00:01 +0200

    cleaning useless classes

--- src/main/java/neo/landscape/theory/apps/pseudoboolean/experiments/DrilsNKLandscapesExperiment.java
+++
@@ -1,8 +0,0 @@
-package neo.landscape.theory.apps.pseudoboolean.experiments;
-
-public class DrilsNKLandscapesExperiment extends ILSRBallPXExperiment {
-    @Override
-    public String getID() {
-        return "drils-nk";
-    }
-}
