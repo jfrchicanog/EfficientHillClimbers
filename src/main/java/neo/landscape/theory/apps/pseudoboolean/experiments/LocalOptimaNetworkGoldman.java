b808ad765af81e900e2afb396f53a5de446dbb38
Francisco Chicano <chicano@lcc.uma.es>
2022-04-20 16:53:55 +0200

    adding new LON experiment and removing .gitignore

---
+++ src/main/java/neo/landscape/theory/apps/pseudoboolean/experiments/LocalOptimaNetworkGoldman.java
@@ -0,0 +1,281 @@
+package neo.landscape.theory.apps.pseudoboolean.experiments;
+
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.IntStream;
+
+import neo.landscape.theory.apps.pseudoboolean.PBSolution;
+import neo.landscape.theory.apps.pseudoboolean.hillclimbers.RBallEfficientHillClimber;
+import neo.landscape.theory.apps.pseudoboolean.hillclimbers.RBallEfficientHillClimberForInstanceOf;
+import neo.landscape.theory.apps.pseudoboolean.hillclimbers.RBallEfficientHillClimberSnapshot;
+import neo.landscape.theory.apps.pseudoboolean.hillclimbers.RBallPBMove;
+import neo.landscape.theory.apps.pseudoboolean.problems.EmbeddedLandscape;
+import neo.landscape.theory.apps.pseudoboolean.problems.MAXSAT;
+import neo.landscape.theory.apps.pseudoboolean.problems.NKLandscapes;
+import neo.landscape.theory.apps.util.Process;
+import neo.landscape.theory.apps.util.Seeds;
+
+public class LocalOptimaNetworkGoldman implements Process {
+    
+    protected EmbeddedLandscape pbf;
+    protected int r;
+    protected RBallEfficientHillClimberSnapshot rball;
+    protected RBallEfficientHillClimberForInstanceOf rballfio;
+    protected long seed;
+    protected String prefix="";
+
+    protected List<RBallPBMove> [] moveBin; 
+    protected long counterValue;
+    protected int [] variableOrder;
+    protected int [] variableRank;
+    //protected int [] counter;
+
+    @Override
+    public String getDescription() {
+        return "This experiment computes the local optima network exploring the search  "
+        + "space using Goldman's algorithm";
+    }
+
+    @Override
+    public String getID() {
+        return "lon-goldman";
+    }
+
+    protected long findLocalOptima() {
+        int n = pbf.getN();
+        int index = n-1;
+        long solutions=0;
+        long nextSolutionsReport = solutions + (1L<<30);
+        PBSolution solution = rball.getSolution();
+        
+        initializeVariableArrays();
+        initializeMoveBin();
+        //counter = new int[n];
+        
+        preparPrefix();
+        
+        long localOptima = 0;
+        
+        int limitIndex = n-prefix.length();
+        
+        index = findNextIndex(index);
+        if (index >= limitIndex) {
+            solutions += (1L << limitIndex);
+        }
+        
+        while (index < limitIndex) {
+            index = findNextIndex(index);
+            if (index < 0) {
+                localOptima++;
+                index = 0;
+            }
+            solutions += (1L << index);
+            if (solutions > nextSolutionsReport) {
+                System.out.println("Solutions explored: "+(double)solutions);
+                System.out.println("Local optima: "+localOptima);
+                nextSolutionsReport = solutions + (1L<<30);
+            }
+            long previousCounterValue = counterValue;
+            counterValue += (1L<<index);
+            index=63-Long.numberOfLeadingZeros(counterValue & ~previousCounterValue);
+            /*
+            while (index < n && counter[index] == 1) {
+                counter[index]=0;
+                //rball.moveOneBit(index);
+                index++;
+            }*/
+            if (index < n) {
+                //counter[index]=1;
+                rball.moveOneBit(variableOrder[index]);
+            }
+            
+        }
+        System.out.println("Total solutions explored: "+solutions);
+        return localOptima;
+    }
+
+    private void preparPrefix() {
+        int index=pbf.getN()-1;
+        for (char c: prefix.toCharArray()) {
+            if (c == '1') {
+                rball.moveOneBit(variableOrder[index]);
+            }
+            index--;
+        }
+    }
+
+    private void initializeVariableArrays() {
+        int n = pbf.getN();
+        int [][] interactions = pbf.getInteractions();
+        variableOrder = IntStream.range(0, n)
+                   .boxed()
+                   //.sorted(Comparator.comparingInt(i->interactions[i].length))
+                   .mapToInt(i->i).toArray(); 
+        variableRank = new int[n];
+        for (int i=0; i < n; i++) {
+            variableRank[variableOrder[i]]=i;
+        }
+    }
+
+    protected int findNextIndex(int index) {
+        while (index >= 0) {
+            for (RBallPBMove move : moveBin[index]) {
+                if (move.improvement > 0) {
+                    return index;
+                }
+            }
+            index--;
+        }
+        return index;
+    }
+    
+    protected void initializeMoveBin() {
+        moveBin = new List[pbf.getN()];
+        for (int i = 0; i < moveBin.length; i++) {
+            moveBin[i] = new ArrayList<RBallPBMove>();
+        }
+        for (RBallPBMove move: rball.iterateOverMoves()) {
+            int min = Integer.MAX_VALUE;
+            for (int subfn : rballfio.subFunctionsAffected(move.flipVariables)) {
+                int [] mask = pbf.getMasks()[subfn];
+                for (int var: mask) {
+                    if (variableRank[var] < min) {
+                        min = variableRank[var];
+                    }
+                }
+            }
+            
+            moveBin[min].add(move);
+            System.out.println("Move "+move+" in "+min);
+        }
+        for (int i=0; i < moveBin.length; i++) {
+            System.out.println("Bin["+i+"]: "+moveBin[i].size());
+        }
+
+    }
+
+    protected void prepareRBallExplorationAlgorithm() {
+        rballfio = (RBallEfficientHillClimberForInstanceOf) new RBallEfficientHillClimber(
+                r).initialize(pbf);
+        PBSolution pbs = new PBSolution(pbf.getN());
+        
+        rball = rballfio.initialize(pbs);
+        rball.setSeed(seed);
+    }
+
+    @Override
+    public String getInvocationInfo() {
+        return "Arguments: " + getID() + " (nk <n> <k> <q> <circular> <r> [<seed> [<prefix>]] | maxsat <instance> <r> [<seed> [<prefix>]])";
+    }
+
+    public void execute(String[] args) {
+        if (args.length < 1) {
+            System.out.println(getInvocationInfo());
+            return;
+        }
+
+        if ("nk".equals(args[0])) {
+            args= Arrays.copyOfRange(args, 1, args.length);
+            pbf = configureNKInstance(args);
+        } else if ("maxsat".equals(args[0])) {
+            args= Arrays.copyOfRange(args, 1, args.length);
+            pbf = configureMaxsatInstance(args);
+        }
+        
+        if (pbf == null) {
+            System.out.println(getInvocationInfo());
+            return;
+        }
+        
+        if (!checkPrefixOK()) {
+            System.err.println("The prefix must be a binary string");
+            return;
+        }
+        
+        prepareRBallExplorationAlgorithm();
+
+        long localOptima = findLocalOptima();
+        System.out.println("Seed: "+seed);
+        System.out.println("Local optima: "+localOptima);
+        
+        if (pbf instanceof NKLandscapes) {
+            reportNKInstanceToStandardOutput();
+        }
+
+        
+    }
+
+    private boolean  checkPrefixOK() {
+        return prefix.chars().allMatch(c->(c >= '0' && c <= '1'));
+    }
+
+    private EmbeddedLandscape configureNKInstance(String[] args) {
+        String n = args[0];
+        String k = args[1];
+        String q = args[2];
+        String circular = args[3];
+        r = Integer.parseInt(args[4]);
+        seed = 0;
+        if (args.length >= 6) {
+            seed = Long.parseLong(args[5]);
+        } else {
+            seed = Seeds.getSeed();
+        }
+        
+        if (args.length >= 7) {
+            prefix = args[6];
+        }
+        
+        return createNKInstance(n, k, q, circular);
+    }
+    
+    private EmbeddedLandscape configureMaxsatInstance(String [] args) {
+        String instance = args[0];
+        r = Integer.parseInt(args[1]);
+        seed = 0;
+        if (args.length >= 3) {
+            seed = Long.parseLong(args[2]);
+        } else {
+            seed = Seeds.getSeed();
+        }
+        
+        if (args.length >= 4) {
+            prefix = args[3];
+        }
+        
+        Properties prop = new Properties();
+        prop.setProperty(MAXSAT.INSTANCE_STRING, instance);
+        MAXSAT maxsat = new MAXSAT();
+        maxsat.setConfiguration(prop);
+        return maxsat;
+    }
+
+    private void reportNKInstanceToStandardOutput() {
+        ((NKLandscapes)pbf).writeTo(new OutputStreamWriter(System.out));
+    }
+    
+    private EmbeddedLandscape createNKInstance(String n, String k, String q, String circular) {
+        NKLandscapes pbf = new NKLandscapes();
+        Properties prop = new Properties();
+        prop.setProperty(NKLandscapes.N_STRING, n);
+        prop.setProperty(NKLandscapes.K_STRING, k);
+
+        if (!q.equals("-")) {
+            prop.setProperty(NKLandscapes.Q_STRING, q);
+        }
+
+        if (circular.equals("y")) {
+            prop.setProperty(NKLandscapes.CIRCULAR_STRING, "yes");
+        }
+
+        pbf.setSeed(seed);
+        pbf.setConfiguration(prop);
+        
+        return pbf;
+    }
+
+}
